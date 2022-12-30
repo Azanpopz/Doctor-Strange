77b59239d6c00861577a2c35c80f3ecff771aba2
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-12-30 22:52:39 +0530

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,6 +1,6 @@
 class script(object):
 
-    AUTO_APPROVE = """𝙷𝙴𝚈 {}\nWelcome To {title}\n\nYour Auto Approved"""
+    AUTO_APPROVE = """𝙷𝙴𝚈 {mention}\nWelcome To {title}\n\nYour Auto Approved"""
     SUR_TXT = """
 <b>
 𝙷𝙴𝙻𝙻𝙾 {},
