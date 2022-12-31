d876fd7fae038a175f47f6f46a012071ee5a13e4
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-01-01 02:08:14 +0530

    Update translate.py

--- plugins/Minnal/translate.py
+++ plugins/Minnal/translate.py
@@ -8,7 +8,12 @@ from pyrogram.types import (
 from plugins.helper.database import find , insert
 from plugins.helper.list import list
 
-
+@Client.on_message(filters.private & filters.command(['translation']))
+async def start(client, message):
+          insert(int(message.chat.id))
+          await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot \n I can translate any language to you selected language__",reply_to_message_id = message.message_id , parse_mode="markdown", reply_markup=InlineKeyboardMarkup(            [                [                    InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ],                 [InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical"),InlineKeyboardButton("How To Use",url = "https://youtu.be/dUYvenXiYKE") ]           ]        ) )
+            
+            
             
 @Client.on_message(filters.command(["language", "code"]) & filters.reply & filters.group)
 async def echo(client, message):
